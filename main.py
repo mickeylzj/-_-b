43b1a7c1a4e6d24e4012004e840febc6a86a5c9e
Cedric-Z <514074577@qq.com>
2016-05-04 22:22:40 +0800

    resolve conflict

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ import keymanage  # get keys
 
 
 def main():
-<<<<<<< HEAD
     pass
-=======
-    with open("./.key/key") as f:
-        key = json.loads(f.read())
-        print(key)
->>>>>>> origin/master
 
 
 if __name__ == '__main__':
